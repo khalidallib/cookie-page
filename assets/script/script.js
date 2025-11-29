1da21ce6f1114b63a836ba5d088366b7331b47a8
Khalid Alli-B <khalidallibalogun08@gmail.com>
2025-11-28 23:45:32 -0600

    added output and callback functions, fixed bugs and types with HTML and JavaScript

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,9 +1,9 @@
 'use strict';
 
 function setCookie(name, value, seconds) {
-  const date = new Date()
+  const date = new Date();
   date.setTime(date.getTime() + (seconds * 1000));
-  document.cookie = `${name} = ${value};expires=${date.toUTCString()};path=/`
+  document.cookie = `${name}=${value};expires=${date.toUTCString()};path=/`;
 }
 
 function getCookie(name) {
@@ -15,28 +15,75 @@ function getCookie(name) {
   return null;
 }
 
+function getBrowserName() {
+  const ua = navigator.userAgent;
+  if (ua.indexOf("Chrome") > -1) return "Chrome";
+  if (ua.indexOf("Firefox") > -1) return "Firefox";
+  if (ua.indexOf("Safari") > -1 && ua.indexOf("Chrome") === -1) return "Safari";
+  if (ua.indexOf("Edge") > -1) return "Edge";
+  return "Unknown";
+}
+
 function getOSName() {
-  const platform = navigator.platform.toLocaleLowerCase();
-  if (platform.indexOf("win") > -1 ) {
-    return "Windows"
-  }
-  if (platform.indexOf("max") > -1 ) {
-    return "MacOS"
-  }
-  if (platform.indexOf("Linux") > -1 ) {
-    return "Linux"
-  }
+  const platform = navigator.platform.toLowerCase();
+  if (platform.indexOf("win") > -1) return "Windows";
+  if (platform.indexOf("mac") > -1) return "MacOS";
+  if (platform.indexOf("linux") > -1) return "Linux";
+  if (/android/i.test(navigator.userAgent)) return "Android";
+  if (/iphone|ipad|ipod/i.test(navigator.userAgent)) return "iOS";
+  return "Unknown";
 }
 
 function getScreenSize() {
-  return `${screen.width}x${screen.height}`
+  return `${screen.width}x${screen.height}`;
 }
 
-window.onload =  function() {
+window.addEventListener("load", () => {
+  const cookieDialog = document.getElementById("cookieDialogue");
+  const settingsDialog = document.getElementById("settingsDialogue");
+
+  const acceptAllBtn = document.getElementById("acceptAll");
+  const openSettingsBtn = document.getElementById("openSettings");
+  const saveBtn = document.getElementById("save");
+
+  const browserToggle = document.getElementById("browserToggle");
+  const osToggle = document.getElementById("osToggle");
+  const screenToggle = document.getElementById("screenToggle");
+
   setTimeout(() => {
-    if(!getCookie("visited")) {
-      document.getElementById("cookieDialog").showModal();
+    if (!getCookie("visited")) {
+      cookieDialog.showModal();
     }
   }, 1000);
-}
 
+  acceptAllBtn.addEventListener("click", () => {
+    setCookie("visited", "true", 20);
+    setCookie("browser", getBrowserName(), 20);
+    setCookie("os", getOSName(), 20);
+    setCookie("screen", getScreenSize(), 20);
+    cookieDialog.close();
+  });
+
+  openSettingsBtn.addEventListener("click", () => {
+    cookieDialog.close();
+    settingsDialog.showModal();
+  });
+
+  saveBtn.addEventListener("click", () => {
+    setCookie("visited", "true", 20);
+    if (browserToggle.checked) {
+      setCookie("browser", getBrowserName(), 20);
+    }
+    if (osToggle.checked) {
+      setCookie("os", getOSName(), 20);
+    }
+    if (screenToggle.checked) {
+      setCookie("screen", getScreenSize(), 20);
+    }
+    settingsDialog.close();
+  });
+});
+
+console.log("Browser:", getCookie("browser"));
+console.log("OS:", getCookie("os"));
+console.log("Screen:", getCookie("screen"));
