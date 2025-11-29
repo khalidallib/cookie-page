7c247099ebe4faecebfd6d087dbcfdef78e79691
Khalid Alli-B <khalidallibalogun08@gmail.com>
2025-11-28 23:18:55 -0600

    added setCookie and getCookie function

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,2 +1,16 @@
 'use strict';
 
+function setCookie(name, value, seconds) {
+  const date = new Date()
+  date.setTime(date.getTime() + (seconds * 1000));
+  document.cookie = `${name} = ${value};expires=${date.toUTCString()};path=/`
+}
+
+function getCookie(name) {
+  const cookies = document.cookie.split(';');
+  for (let myCookie of cookies) {
+    let [key, val] = myCookie.trim().split('=');
+    if (key === name) return val;
+  }
+  return null;
+}
